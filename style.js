8c6f7735a4dbe5d1ab1da27089dfe11fcbd2e022
Abdo <abdo.el.kabli12@gmail.com>
2025-11-05 12:27:53 +0100

    Market version 1.1 / Home version 2.0

---
+++ style.js
@@ -0,0 +1,24 @@
+// let i = 0;
+
+// let cards = document.getElementsById('id${i}');
+
+// fetch('pokemondata1.json')
+//   .then(res => {
+//     if (!res.ok) {
+//       throw new Error('Network err' + res.status);
+//     }
+//     return res.json();
+//   })
+
+//   .then(data => {
+//     console.log('file content:', data)
+//   })
+//   .catch(error => {
+//         console.error('There was a problem fetching the file:', error);
+//   });
+
+// for( i = 0; i >= 12; i++) {
+//   cards.forEach(element => {
+    
+//   });
+// }
